0c7679e0600afd2dc42334f193dc37fea4081418
Vašek Potoček <vasek.potocek@post.cz>
2020-10-16 14:39:34 +0200

    Settings

--- odhad.js
+++ odhad.js
@@ -1,10 +1,10 @@
 let startTime;
-let msm;
+let msm, conf;
 
 function start() {
   document.getElementById('b').disabled = true;
   document.body.setAttribute('data-state', 'waiting');
-  let wait = 3000 * (Math.random() + 1);
+  let wait = (2*Math.random() + 1) * conf.wait * 1000;
   window.setTimeout(run, wait);
 }
 
@@ -17,8 +17,8 @@ function run() {
 function stop() {
   document.body.setAttribute('data-state', 'base');
   let time = Date.now();
-  let diff = time - startTime - 1000;
-  addLine(time, diff);
+  let diff = time - startTime - conf.goal * 1000;
+  addLine(time, conf.goal, diff);
 }
 
 function click() {
@@ -28,14 +28,14 @@ function click() {
     start();
 }
 
-function addLine(time, diff, update = true) {
+function addLine(time, goal, diff, update = true) {
   let res = document.getElementById('r');
   let diffText = (diff > 0 ? '+' : '') + diff.toString();
   let cls = diff > 10 ? 'plus' : diff < -10 ? 'minus' : 'spoton';
   let now = new Date(time).toLocaleString();
-  res.innerHTML = `${now}\t5s\t<span class="${cls}">${diffText} ms</span>\n` + res.innerHTML;
+  res.innerHTML = `${now}\t${goal}s\t<span class="${cls}">${diffText} ms</span>\n` + res.innerHTML;
   if(update) {
-    msm.push({time, diff});
+    msm.push({time, goal, diff});
     localStorage['odhad'] = JSON.stringify(msm);
   }
 }
@@ -43,10 +43,36 @@ function addLine(time, diff, update = true) {
 function loadLS() {
   msm = JSON.parse(localStorage['odhad'] || '[]');
   for(m of msm)
-    addLine(m.time, m.diff, false);
+    addLine(m.time, m.goal || 5, m.diff, false);
+
+  conf = JSON.parse(localStorage['odhad-conf'] || '{}');
+  document.getElementById('iGoal').value = conf.goal || 5;
+  document.getElementById('iWait').value = conf.wait || 1;
+  inputGoal(document.getElementById('iGoal'));
+  inputWait(document.getElementById('iWait'));
+}
+
+function inputGoal(elm) {
+  let val = elm.value;
+  elm.labels[1].innerText = `${val} s`;
+  if(conf.goal != val) {
+    conf.goal = val;
+    localStorage['odhad-conf'] = JSON.stringify(conf);
+  }
+}
+
+function inputWait(elm) {
+  let val = elm.value;
+  elm.labels[1].innerText = val == 0 ? '0 s' : `${val} – ${3*val} s`;
+  if(conf.wait != val) {
+    conf.wait = val;
+    localStorage['odhad-conf'] = JSON.stringify(conf);
+  }
 }
 
 window.addEventListener('DOMContentLoaded', function() {
-  loadLS();
   document.getElementById('b').addEventListener('click', click);
+  document.getElementById('iGoal').addEventListener('input', e => inputGoal(e.currentTarget));
+  document.getElementById('iWait').addEventListener('input', e => inputWait(e.currentTarget));
+  loadLS();
 });
